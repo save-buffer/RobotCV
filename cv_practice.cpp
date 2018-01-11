0e81f40d8c45b35635ac33e3f49c13e0f56ebdd1
save-buffer <warplane@hotmail.com>
2018-01-11 13:39:14 -0800

    Initial

---
+++ cv_practice.cpp
@@ -0,0 +1,136 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+//For compatibility with opencv2
+namespace cv
+{
+    using std::vector;
+}
+
+//./cv_practice /mnt/c/Users/Sasha/Downloads/tennisball2.jpg
+
+Mat threshold_image(Mat img)
+{
+    Mat hsv(img.rows, img.cols, CV_8UC3);
+    cvtColor(img, hsv, CV_BGR2HSV);
+    Mat thresh(img.rows, img.cols, CV_8UC1);
+    inRange(hsv, Scalar(0.11 * 256, 0.60 * 256, 0.20 * 256), Scalar(0.14 * 256, 1.0 * 255.0, 1.0 * 256), thresh);
+    return(thresh);
+}
+
+Mat morphed_img(Mat mask)
+{
+    Mat se21 = getStructuringElement(MORPH_RECT, Size(21, 21));
+    Mat se11 = getStructuringElement(MORPH_RECT, Size(11, 11));
+
+    morphologyEx(mask, mask, MORPH_CLOSE, se21);
+    morphologyEx(mask, mask, MORPH_OPEN, se11);
+
+    GaussianBlur(mask, mask, Size(15, 15), 0, 0);
+    return(mask);   
+}
+
+Mat overall_filter(Mat img)
+{
+    Mat mask = threshold_image(img);
+    Mat filtered = morphed_img(mask);
+    return(filtered);
+}
+
+void hough_circles_identifier(Mat src)
+{
+    Mat hough_in = overall_filter(src);
+    vector<Vec3f> circles;
+    /// Apply the Hough Transform to find the circles
+    HoughCircles(hough_in, circles, CV_HOUGH_GRADIENT, 1.1, hough_in.rows/10, 100, 40, 0, 0);
+
+    printf("circles: %lu\n", circles.size());
+    /// Draw the circles detected
+    for(size_t i = 0; i < circles.size(); i++)
+    {
+	Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+	int radius = cvRound(circles[i][2]);
+	// circle center
+	circle(src, center, 3, Scalar(0,255,0), -1, 8, 0);
+	// circle outline
+	circle(src, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+    }
+
+    /// Show your results
+    namedWindow("Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE);
+    imshow("Hough Circle Transform Demo", hough_in);
+}
+
+/*
+  take your post-processed images and get the connected components, draw a bounding square around them,
+  take the inscribed circle of the bounding square, then calculate a coverage overlap
+  that will give you a "percent like a circle" metric
+  and you can tune that threshold to whatever is best for your application
+ */
+void connected_components_identifier(Mat src)
+{
+    Mat filtered = overall_filter(src);
+    Mat labels;
+    int components = connectedComponents(filtered, labels);
+    printf("%d connected components\n", components);
+    vector<Rect> rectangles;
+    for(int i = 1; i < components; i++)
+    {
+	Mat component_i;
+	inRange(labels, Scalar(i), Scalar(i), component_i);
+	Rect r = boundingRect(component_i);
+	
+	Point center = (r.tl() + r.br()) / 2;
+	int radius = abs(r.tl().y - center.y);
+	Mat circ(component_i.size(), component_i.type());
+	circ = Scalar(0, 0, 0);
+	circle(circ, center, radius, i, -1);
+
+	Mat intersection;
+	bitwise_and(circ, component_i, intersection);
+	Mat union_;
+	bitwise_or(circ, component_i, union_);
+
+	printf("intersection: %d\n", countNonZero(intersection));
+	printf("union: %d\n", countNonZero(union_));
+
+	float iou = (float)countNonZero(intersection) / (float)countNonZero(union_);
+
+	printf("%.3f iou\n", iou);
+
+#define CIRCLE_THRESH 0.8f
+
+	if(iou >= CIRCLE_THRESH)
+	    rectangles.push_back(r);
+
+#undef CIRCLE_THRESH
+    }
+
+    for(Rect r : rectangles)
+    {
+	rectangle(src, r.tl(), r.br(), Scalar(255, 0, 0), 1);
+    }
+    
+    normalize(labels, labels, 0, 255, NORM_MINMAX, CV_8U);
+    namedWindow("Connected Components Transform", CV_WINDOW_AUTOSIZE);
+    imshow("Connected Components Transform", src);
+}
+
+int main(int argc, char** argv)
+{
+    Mat src;
+
+    /// Read the image
+    src = imread(argv[1], 1);
+    
+    if( !src.data )
+	return -1; 
+
+//    hough_circles_identifier(src);
+    connected_components_identifier(src);
+    waitKey(0);
+    return 0;
+}
